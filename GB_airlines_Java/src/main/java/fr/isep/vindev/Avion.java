f44f040645629b642465b76395dddfd3c9140de8
VINCENT Gabriel <vincentgab78@gmail.com>
2024-12-05 10:56:10 +0100

    classes Volantes
    Vol, Avion, Aeroport

--- GB_airlines_Java/src/main/java/fr/isep/vindev/Avion.java
+++ GB_airlines_Java/src/main/java/fr/isep/vindev/Avion.java
@@ -1,20 +1,47 @@
 package fr.isep.vindev;
 
 public class Avion {
-    private static int immatriculation;
-    private static String modele;
-    private static int capacite;
+    private String immatriculation;
+    private String modele;
+    private int capacite;
 
-    public static int getImmatriculation() {
+    public Avion(String immatriculation, String modele, int capacite) {
+        this.immatriculation = immatriculation;
+        this.modele = modele;
+        this.capacite = capacite;
+    }
+
+    public String getImmatriculation() {
         return immatriculation;
+    }
+
+    public void setImmatriculation(String immatriculation) {
+        this.immatriculation = immatriculation;
     }
 
     public String getModele() {
         return modele;
     }
 
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
     public int getCapacite() {
         return capacite;
     }
-}
 
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    @Override
+    public String toString() {
+        return "Avion [Immatriculation:" + immatriculation + ", modele:" + modele + ", capacite:" + capacite;
+    }
+
+    public void affecterVol(Vol vol){
+        vol.setAvion(this);
+    }
+
+}
--- GB_airlines_Java/src/main/java/fr/isep/vindev/Vol.java
+++ GB_airlines_Java/src/main/java/fr/isep/vindev/Vol.java
@@ -1,5 +1,97 @@
 package fr.isep.vindev;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Vol {
     private int numeroVol;
+    private Avion avion;
+    private ArrayList<Employe> equipage;
+    private ArrayList<Passager> passager;
+    private Aeroport origine;
+    private Aeroport destination;
+    private Date dateHeureDepart;
+    private Date dateHeureArrivee;
+    private boolean etat;
+    private boolean isComplete;
+
+    public Vol(int numeroVolDate, Date dateHeureDepart, Date dateHeureArrivee) {
+        this.numeroVol = numeroVol;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrivee = dateHeureArrivee;
+    }
+
+    public int getNumeroVol() {
+        return numeroVol;
+    }
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public void setAvion(Avion avion) {
+        this.avion = avion;
+    }
+
+    public Aeroport getOrigine() {
+        return origine;
+    }
+
+    public void setOrigine(Aeroport origine) {
+        this.origine = origine;
+    }
+
+    public Aeroport getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Aeroport destination) {
+        this.destination = destination;
+    }
+
+    public Date getDateHeureDepart() {
+        return dateHeureDepart;
+    }
+
+    public void setDateHeureDepart(Date dateHeureDepart) {
+        this.dateHeureDepart = dateHeureDepart;
+    }
+
+    public Date getDateHeureArrivee() {
+        return dateHeureArrivee;
+    }
+
+    public void setDateHeureArrivee(Date dateHeureArrivee) {
+        this.dateHeureArrivee = dateHeureArrivee;
+    }
+
+    public boolean isEtat() {
+        return etat;
+    }
+
+    public void setEtat(boolean etat) {
+        this.etat = etat;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    @Override
+    public String toString() {
+        return "Vol [numero:" + numeroVol + ", avion:" + avion + ", depart:" + origine + ", arrivee:" + destination + ", heure de depart:" + dateHeureDepart + ", heure d'arrivee:" + dateHeureArrivee + ", etat:" + etat + ", complet: " + isComplete + "]";
+    }
+
+    public void planifierVol(){
+
+    }
+
+    public void annulerVol(int numeroVol){
+
+    }
 }
+
